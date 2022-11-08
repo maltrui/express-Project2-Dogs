3214037fc9552c76ac3c365211d1a259a27ccf4a
maltrui <mcaltrui@gmail.com>
2022-11-07 19:51:15 -0500

    Add API and random dog photo

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,27 @@ var express = require("express");
 var router = express.Router();
 var passport = require("passport");
 
+const fetch = require('node-fetch');
+const rootUrl = 'https://random.dog/woof.json'
+
 // import private route middleware
 const isLoggedIn = require("../config/auth");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Home / Public Page" });
+  let dogPhoto
+  fetch(rootUrl)
+    .then((res)=> res.json())
+    .then(dog =>{
+      console.log(dog.url)
+      dogPhoto = dog.url
+      res.render("index", { title: "Pick A Dog", dogPhoto });
+    })
+
 });
 
-router.get("/private", isLoggedIn, function (req, res) {
-  res.render("private", { title: "Private Page" });
+router.get("/post", isLoggedIn, function (req, res) {
+  res.render("post", { title: "Posts"});
 });
 
 // OAuth Routes
